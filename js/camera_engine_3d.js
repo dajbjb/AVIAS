6e7288eb96ec6830739084113f0e4cf8f7bd4fc9
dajbjb <wbddwd55@gmail.com>
2026-01-10 18:42:41 +0200

    Create camera_engine_3d.js

---
+++ js/camera_engine_3d.js
@@ -0,0 +1,10 @@
+/**
+ * 3D CAMERA ENGINE (DEPRECATED)
+ * -------------------
+ * This file is no longer in use.
+ * The camera functionality has been moved to camera.js
+ * 
+ * This file is kept for reference only.
+ */
+
+console.log("camera_engine_3d.js is deprecated and not in use.");
